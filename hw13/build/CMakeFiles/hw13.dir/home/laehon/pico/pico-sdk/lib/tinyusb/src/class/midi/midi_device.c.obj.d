b9593196995ed30e9810d45a550b888bfccc7c30
laehon <leonwang.lol@gmail.com>
2024-05-11 13:34:35 -0500

    added hw13

---
+++ hw13/build/CMakeFiles/hw13.dir/home/laehon/pico/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c.obj.d
@@ -0,0 +1,6 @@
+CMakeFiles/hw13.dir/home/laehon/pico/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c.obj: \
+ /home/laehon/pico/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c \
+ /home/laehon/pico/pico-sdk/lib/tinyusb/src/tusb_option.h \
+ /home/laehon/pico/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
+ /home/laehon/me433/hw13/tusb_config.h \
+ /home/laehon/pico/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
